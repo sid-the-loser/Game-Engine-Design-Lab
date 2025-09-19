7cd7072295374e95398e32ff19385bca786ad835
Sidharth <sidharthsuresh07@gmail.com>
2025-09-19 16:46:07 -0400

    asdfasdfasdf

--- lab-3/scripts/player.gd
+++ lab-3/scripts/player.gd
@@ -3,3 +3,11 @@
 # the player.
 
 extends CharacterBody2D
+
+@export var max_movement_speed: float = 1000
+@export var movement_lerp: float = 2
+
+var movement_direction: Vector2 = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	velocity = velocity.lerp(movement_direction, movement_lerp * delta)
